a6a2e35c85cca1d64139ac1eab8d19cd8d55e306
Павел <freestalker1987@gmail.com>
2016-11-28 19:52:49 +0300

    Project:
    DAO started
    sync

--- FinalProject/src/main/java/DAO/DAO.java
+++ FinalProject/src/main/java/DAO/DAO.java
@@ -1,4 +1,4 @@
-package dao;
+package DAO;
 
 import domain.Entity;
 import exeptions.PersistExeption;
--- FinalProject/src/main/java/test/main.java
+++ FinalProject/src/main/java/test/main.java
@@ -1,10 +1,5 @@
 package test;
 
-import dao.CityMySQLDAO;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Duration;
-
 /**
  * Created by Павел on 28-Nov-16.
  */
